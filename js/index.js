a94482464cb7e50dca1d7e35d780408aac5a5ff1
MSI\moneo <jvr.moneo@gmail.com>
2020-10-01 15:28:19 +0200

    Añadir más pruebas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
 
-const item2 = document.getElementsByClassName('link-section');
+// const item2 = document.getElementsByClassName('link-section');
 
-console.log(item2);
+// // console.log(item2);
+// // console.log('item2[1]', item2[1].innerHTML);
 
-console.log('item2[1]', item2[1].innerHTML);
+// const item3 = document.getElementsByTagName('p');
+// console.log(item3);
+
+// console.log(item3[1]);
+
+// // cambiamos su contenido
+// item3[1].innerText='Hola que tal'
+
+//-----------
+// const header = document.getElementsByTagName('header');
+// const name = prompt('Dime tu nombre');
+// if(name !== ''){
+//     header[0].innerHTML=`<h1>Bienvenido a mi página ${name}</h1>`;
+// }
+// console.log(header);
+//-----------
+
+// SELECTOR POR ID, CLASE, O ETIQUETA querySelector
+// const myElememnt = document.querySelector('.link-section');//solo me ha seleccionado el primer elmento
+// const myElementById = document.querySelector('#featured');
+
+// console.log(myElememnt);
+//console.log(myElementById);
+
+//querySelectorAll
+// const myElementByClass = document.querySelectorAll('link-section');
+// console.log(myElementByClass);
+
+// const myElementById = document.querySelector('#featured');
+// myElementById.style.color='red';
+
+// const featuredArticle = document.getElementById('featured');
+// featuredArticle.className = 'featured-article';
+
